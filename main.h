2765d45954558f48b803dc02420e261636070593
Samuel Nzubechi Chukwuma <98186867+denzubechi@users.noreply.github.com>
2022-07-12 17:59:29 -0700

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,23 @@
 #ifndef HEADER_MAIN
 #define HEADER_MAIN
 #include <stdarg.h>
+
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
+ *
+ * Return: int
+ */
+typedef struct structprint
+{
+	char *q;
+	int (*u)(char *format, va_list);
+} structype;
+
 int _putchar(char c);
 void _puts(char *str);
 int _strlen(char *s);
