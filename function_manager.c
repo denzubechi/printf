303ade0e185834f0d1e8ae697b4e81828ea17947
Samuel Nzubechi Chukwuma <98186867+denzubechi@users.noreply.github.com>
2022-07-12 18:41:16 -0700

    Update function_manager.c

--- function_manager.c
+++ function_manager.c
@@ -1,39 +1,68 @@
-#include <unistd.h>
+#include "main.h"
+#include <stdarg.h>
 /**
- * _memcpy - copies memory area
- * @dest: pointer to destination memory area
- * @src: pointer to source memory area
- * @n: number of bytes to copy
- * @bufferlen: current number of bytes taken by buffer
- * Return: pointer to destination memory area
+ *function_manager - function manager
+ *@c: character to find
+ *@arg: va_list type
+ *Description: This function call other functions
+ * when the character is found
+ *Return: count of printed characters
  */
-char *_memcpy(char *dest, char *src, unsigned int n, unsigned int bufferlen)
+int function_manager(char c, va_list arg)
 {
-	unsigned int i;
+	int cont = 0;
 
-	for (i = 0; i < n; i++)
-		dest[i + bufferlen] = src[i];
-	return (dest);
+	cont = _switch(c, arg);
+	if (c == 'p')
+		cont = print_ptr(arg);
+	return (cont);
 }
 /**
- * _strlen - gets length of the string
- * @s: input string
- * Return: length of string
+ *_switch - switch
+ *@c: character to find
+ *@arg: va_list type
+ *Description: This function evaluate cases
+ *Return: count of printed characters
  */
-int _strlen(char *s)
+int _switch(char c, va_list arg)
 {
-	int i;
+	int cont = 0;
 
-	for (i = 0; *(s + i); i++)
-		;
-	return (i);
-}
-/**
- * _puts - print a buffer.
- * @buffer: buffer passed
- * @size: number of bytes to print
- */
-void _puts(char *buffer, int size)
-{
-	write(1, buffer, size);
+	switch (c)
+	{
+		case 'b':
+			cont += print_unsign(arg, 2);
+			break;
+		case 'c':
+			cont += print_character(arg);
+			break;
+		case 'd':
+		case 'i':
+			cont += print_sign(arg, 10);
+			break;
+		case 'o':
+			cont += print_unsign(arg, 8);
+			break;
+		case 'r':
+			cont += print_rev(arg);
+			break;
+		case 'R':
+			cont += print_rot13(arg);
+			break;
+		case 's':
+			cont += print_string(arg);
+			break;
+		case 'u':
+			cont += print_unsign(arg, 10);
+			break;
+		case 'x':
+			cont += print_base16_upper_lower(arg, "0123456789abcdef");
+			break;
+		case 'X':
+			cont += print_base16_upper_lower(arg, "0123456789ABCDEF");
+			break;
+		default:
+			cont = -1;
+	}
+	return (cont);
 }
