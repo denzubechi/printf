1fdd5a725faf867ae5667d350273ad9c746da4c0
Samuel Nzubechi Chukwuma <98186867+denzubechi@users.noreply.github.com>
2022-07-12 18:13:18 -0700

    Update main.h

--- main.h
+++ main.h
@@ -1,39 +1,35 @@
-#ifndef HEADER_MAIN
-#define HEADER_MAIN
+#ifndef MAIN_H
+#define MAIN_H
+
+#define BUFSIZE 1025
 #include <stdarg.h>
 
-#include <stddef.h>
-#include <stdlib.h>
-
 /**
- * struct structprint - structure containing
- * @q: the location and method to translate data to characters.
- * @u: print function for specific type.
- *
- * Return: int
- */
-typedef struct structprint
+  * struct validTypes - structure to lookup functions for valid types
+  * @valid: flags are preceded by a '%' character.
+  * @f: pointer to function
+  */
+typedef struct validTypes
 {
-	char *q;
-	int (*u)(char *format, va_list);
-} structype;
+	char *valid;
+	char *(*f)();
+} v_types;
 
-int _putchar(char c);
-void _puts(char *str);
-int _strlen(char *s);
 int _printf(const char *format, ...);
-char *convert_to(char representation[], unsigned int num, int base);
-int function_manager(char c, va_list arg);
-int _switch(char c, va_list arg);
-int print_character(va_list arg);
-int print_sign(va_list arg, int base);
-int print_unsign(va_list arg, int base);
-int print_string(va_list arg);
-int print_ptr(va_list arg);
-int print_rot13(va_list arg);
-int print_rev(va_list arg);
-int print_base16_upper_lower(va_list arg, char *representation);
-int loop_format(va_list arg, const char *string);
-int call_function_manager(char aux, va_list arg);
-int check_percent(int *flag, char aux);
+char *(*get_valid_type(char s))(va_list);
+char *found_char(va_list c);
+char *found_string(va_list *s);
+char *found_percent();
+char *found_int(va_list n);
+char *found_unsigned(va_list usign);
+char *found_nothing(char);
+char *found_reverse(va_list s);
+char *found_rot13(va_list s);
+char *found_octal(va_list n);
+char *_memcpy(char *dest, char *src, unsigned int n, unsigned int bufferlen);
+int _strlen(char *s);
+void _puts(char *buffer, int size);
+int alloc_buffer(char *hold, int hlen, char *buffer, int blen, double *total);
+char *ctos(char c);
+
 #endif
